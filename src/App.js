b52c3d2b63396337b3fc69af0cd5ea2f9c6eb180
Aleksey6453 <login6453@gmail.com>
2023-03-22 23:08:00 +0200

    commit about start of markup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@
 
 function App() {
   return (
-    <div>
-      <h1>27</h1>    
+    <div className='main wrapper'>
+      <header className="header">
+        <div className="header_left">
+          <img src="/img/logo.png" alt="logo" className="logo" />
+          <div>
+            <h3>
+              Sneakers Store
+            </h3>
+            <p>
+              Text about this store
+            </p>
+          </div>
+        </div>
+        <ul className="header_right">
+           <li className="flex_li"> <svg className="flex_li_half" /> <span className="flex_li_half"> 1205 $</span> </li> 
+           <li> <img src="" alt="face" /> </li>
+        </ul>
+      </header>  
+      <div className="content">
+        <h1>All sneakers</h1>
+      </div>           
     </div>
   );
 }
