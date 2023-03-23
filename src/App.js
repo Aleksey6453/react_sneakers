2c92a5220a5b803f9e275c28e23219f3fde4bc63
Aleksey6453 <login6453@gmail.com>
2023-03-23 19:47:36 +0200

    c about markup of header

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ function App() {
       <header className="header">
         <div className="header_left">
           <img src="/img/logo.png" alt="logo" className="logo" />
-          <div>
-            <h3>
-              Sneakers Store
+          <div className="padding_l_10">
+            <h3 className="title_header m_p_0">
+              Sneakers Store 97
             </h3>
-            <p>
-              Text about this store
+            <p className="m_p_0">
+              The store is dedicated to the air max 97 
             </p>
           </div>
         </div>
         <ul className="header_right">
-           <li className="flex_li"> <svg className="flex_li_half" /> <span className="flex_li_half"> 1205 $</span> </li> 
-           <li> <img src="" alt="face" /> </li>
+           <li className="flex_li"> <img src="/img/basket.png" alt="basket" className="icon"/>  </li> 
+           <li className="padding_l_10"><span className="flex_li_half"> 1205 $</span></li>
+           <li> <img src="/img/user.png" alt="face" className="icon padding_l_10"/> </li>
         </ul>
       </header>  
       <div className="content">
